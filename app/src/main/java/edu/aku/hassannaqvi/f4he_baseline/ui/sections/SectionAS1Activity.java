ea52c18907200f26a8f57bf3dbb14b4044d2e2f4
ahs1n <ahsanpervez74@gmail.com>
2021-11-09 13:18:38 +0500

    Added SectionAS1Activity

---
+++ app/src/main/java/edu/aku/hassannaqvi/f4he_baseline/ui/sections/SectionAS1Activity.java
@@ -0,0 +1,106 @@
+package edu.aku.hassannaqvi.f4he_baseline.ui.sections;
+
+import static edu.aku.hassannaqvi.f4he_baseline.core.MainApp.form;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.json.JSONException;
+
+import edu.aku.hassannaqvi.f4he_baseline.R;
+import edu.aku.hassannaqvi.f4he_baseline.contracts.TableContracts;
+import edu.aku.hassannaqvi.f4he_baseline.core.MainApp;
+import edu.aku.hassannaqvi.f4he_baseline.database.DatabaseHelper;
+import edu.aku.hassannaqvi.f4he_baseline.databinding.ActivitySectionAs1Binding;
+import edu.aku.hassannaqvi.f4he_baseline.ui.EndingActivity;
+
+public class SectionAS1Activity extends AppCompatActivity {
+    private static final String TAG = "SectionAS1Activity";
+    ActivitySectionAs1Binding bi;
+    private DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_as1);
+        bi.setCallback(this);
+        bi.setForm(form);
+        setSupportActionBar(bi.toolbar);
+        db = MainApp.appInfo.dbHelper;
+        setupSkips();
+    }
+
+    private void setupSkips() {
+    }
+
+
+    private boolean insertNewRecord() {
+        if (!form.getUid().equals("")) return true;
+        long rowId = 0;
+        try {
+            rowId = db.addForm(form);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this, R.string.db_excp_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        form.setId(String.valueOf(rowId));
+        if (rowId > 0) {
+            form.setUid(form.getDeviceId() + form.getId());
+            db.updatesFormColumn(TableContracts.FormsTable.COLUMN_UID, form.getUid());
+            return true;
+        } else {
+            Toast.makeText(this, R.string.upd_db_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    private boolean updateDB() {
+        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+        int updcount = 0;
+        try {
+            updcount = db.updatesFormColumn(TableContracts.FormsTable.COLUMN_SF2, form.sF2toString());
+        } catch (JSONException e) {
+            Toast.makeText(this, R.string.upd_db + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, R.string.upd_db_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+    private void saveDraft() {
+    }
+
+
+    public void btnContinue(View view) {
+        if (!formValidation()) return;
+        if (!insertNewRecord()) return;
+        saveDraft();
+        if (updateDB()) {
+            finish();
+            startActivity(new Intent(this, SectionAS2Activity.class).putExtra("complete", true));
+        } else Toast.makeText(this, R.string.fail_db_upd, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void btnEnd(View view) {
+        finish();
+        startActivity(new Intent(this, EndingActivity.class).putExtra("complete", false));
+    }
+}
