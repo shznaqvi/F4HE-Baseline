715b39b5f50bd005031ea4eec0dc7e26869e18e5
ahs1n <ahsanpervez74@gmail.com>
2022-01-31 17:26:10 +0500

    *commit

--- app/src/main/java/edu/aku/hassannaqvi/f4he_baseline/ui/IdentificationActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/f4he_baseline/ui/IdentificationActivity.java
@@ -149,7 +149,7 @@ public class IdentificationActivity extends AppCompatActivity {
                 for (Villages v : villages) {
                     if (user.getUserName().contains("user")) {
                         // Only populate test clusters for test users
-                        if (MainApp.selectedPSU.charAt(0) == '9') {
+                        if (v.getPsucode().charAt(0) == '9') {
                             villageNames.add(v.getVillage());
                             villageCodes.add(v.getVcode());
                             psuCode.add(v.getPsucode());
@@ -157,7 +157,7 @@ public class IdentificationActivity extends AppCompatActivity {
 
                     } else {
                         // Only populate actual clusters for actual users (NOT equals 9)
-                        if (MainApp.selectedPSU.charAt(0) == '9') {
+                        if (v.getPsucode().charAt(0) == '9') {
                             villageNames.add(v.getVillage());
                             villageCodes.add(v.getVcode());
                             psuCode.add(v.getPsucode());
